a70ba7a50a352ff6ca6e59559d8fb24536be1535
Mani <55637028+Joker-commits@users.noreply.github.com>
2020-06-30 10:21:05 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ export KBUILD_BUILD_USER="Joker"
 export KBUILD_BUILD_HOST="JK"
 export CROSS_COMPILE=/home/followurluv/kernel/toolchain/gcc-4.9/arm64/bin/aarch64-linux-android-
 export CROSS_COMPILE_ARM32=/home/followurluv/kernel/toolchain/gcc-4.9/arm/bin/arm-linux-androideabi-
-export KBUILD_COMPILER_STRING=$(/home/followurluv/kernel/toolchain/clang/bin/clang --version | head -n 1 | perl -pe 's/\(http.*?\)//gs' | sed -e 's/  */ /g' -e 's/[[:s$
+export KBUILD_COMPILER_STRING=$(/home/followurluv/kernel/toolchain/clang/bin/clang --version | head -n 1 | perl -pe 's/\(http.*?\)//gs' | sed -e 's/  */ /g' -e 's/[[:space:]]*$//')
 
 
 mkdir -p out
